d097364df29ddcd0d961c5e9bc417d0d81df5e7c
sakine mazlomyar <mazlomyar12@gmail.com>
2020-01-16 13:15:04 +0100

    added build files

---
+++ frontend/build/precache-manifest.71fd4d6c327541fc0910974f5bf8d7fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3b0d70f7331d23b4980e7206755fad7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be64c48224a26dd90a44",
+    "url": "/static/css/main.e7cfb751.chunk.css"
+  },
+  {
+    "revision": "3ea7fb9c1d38a64a5777",
+    "url": "/static/js/2.0d61810a.chunk.js"
+  },
+  {
+    "revision": "be64c48224a26dd90a44",
+    "url": "/static/js/main.12f6409a.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  },
+  {
+    "revision": "1734ba8500fe88aa0e8c85018003d3ba",
+    "url": "/static/media/mainView.1734ba85.jpg"
+  },
+  {
+    "revision": "349650ff21f3c319aeb211980f5b4cf1",
+    "url": "/static/media/support.349650ff.jpg"
+  }
+]);
